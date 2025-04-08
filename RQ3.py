2788f1f0fe040e3fc76b8dd3bf8eee93e56a2b54
Sai Pusuluri <99237847+SaiUbc@users.noreply.github.com>
2025-04-07 17:03:37 -0700

    Update RQ3.py

--- RQ3.py
+++ RQ3.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import sqlite3
 
-# Load CSV files
 df1 = pd.read_csv("mortality.csv")
 df2 = pd.read_csv("telehealth.csv")
 
-# Create or connect to SQLite database
 conn = sqlite3.connect("my_database.db")
 
-# Write dataframes to SQLite
 df1.to_sql("mortality", conn, if_exists="replace", index=False)
 df2.to_sql("telehealth", conn, if_exists="replace", index=False)
 
@@ -27,7 +24,6 @@ ORDER BY total_users DESC;
 results = pd.read_sql_query(query, conn)
 print("🔹 Top digital healthcare users by Age Group and Race (2020–2024):")
 print(results)
-
 results.to_csv("top_telehealth_users_by_age_race_3RQ.csv", index=False)
 
 conn.close()
